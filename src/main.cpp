c3dc0855efae504042604933d515f5b10a35ed96
LemonHaze420 <35783139+LemonHaze420@users.noreply.github.com>
2022-12-28 06:54:38 +0000

    Added count_to_100 script
    Moved scripts into their own directory

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@ int main(int argc, char **argp) {
 
     VirtualMachine vm;
     if (strstr(argp[1], "-t")) {
-        vm.compile("program.s", "program.bin");
-        vm.translate_to_x64_asm("program.bin", "program.x64.s");
-        vm.decompile("program.bin", "program.bin.dec");
-        vm.load("program.bin");
-        vm.start();
+        vm.compile("scripts\\count_to_100.mvms", "bin\\count_to_100.mvmb");
+        vm.translate_to_x64_asm("bin\\count_to_100.mvmb", "bin\\count_to_100.x64.s");
+        vm.decompile("bin\\count_to_100.mvmb", "bin\\count_to_100.dec.mvms");
+        if (vm.load("bin\\count_to_100.mvmb"))
+            vm.start();
         return 1;
     }
 
@@ -18,6 +18,7 @@ int main(int argc, char **argp) {
     if (strstr(argp[1], "-c") || strstr(argp[1], "-d"))
         bRun = false;
     
+    int res = 0;
     std::string in = argp[bRun ? 1 : 2];
     if (!bRun) {
         std::string out = argp[3];
@@ -27,8 +28,12 @@ int main(int argc, char **argp) {
         else
             vm.decompile(in, out);
     } else {
-        vm.load(in);
-        vm.start();
+        res = vm.load(in);
+        if (res)
+            vm.start();
+        else {
+            cerr << "Could not load MVMB '" << in << "'!\n";
+        }
     }
-    return 0;
+    return res;
 }
